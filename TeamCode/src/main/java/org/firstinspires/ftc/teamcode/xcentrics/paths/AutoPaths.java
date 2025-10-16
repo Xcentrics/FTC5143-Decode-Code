9c5bf25211e98ca8a297947483bbb0c22cdbd852
Xcentrics <150195241+Xcentrics@users.noreply.github.com>
2025-10-16 17:02:11 -0500

    h

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/paths/AutoPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/paths/AutoPaths.java
@@ -1,20 +1,25 @@
 package org.firstinspires.ftc.teamcode.xcentrics.paths;
 
+import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 
-public class redAutoPaths {
+import org.firstinspires.ftc.teamcode.xcentrics.subsystem.Camera;
+
+public class AutoPaths {
 private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(72, 20, Math.toRadians(115)); // Scoring Pose of our robot. It is facing the goal at a 115 degree angle.
+    private final Pose scorePoseBlue = new Pose(84, 84, Math.toRadians(50)); // Scoring Pose of our robot. It is facing the goal at a 115 degree angle.
+    private final Pose scorePoseRed = new Pose(60,82,Math.toRadians(130));
     private final Pose PPGPose = new Pose(100, 83.5, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
     private final Pose PGPPose = new Pose(100, 59.5, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
     private final Pose GPPPose = new Pose(100, 35.5, Math.toRadians(0)); // Lowest (Third Set) of Artifacts from the Spike Mark.
 
-    private PathChain getPPG;
+    private PathChain grabPPG;
     private PathChain scorePPG;
-    private PathChain getPGP;
-    private Pathchain scoreGPG;
-    private PathChain getGPP;
+    private PathChain grabPGP;
+    private PathChain scorePGP;
+    private PathChain grabGPP;
     private PathChain scoreGPP;
     private Follower follower;
     
@@ -22,12 +27,14 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
     private int pathStatePGP; // Current state machine value
     private int pathStateGPP; // Current state machine value
     private Camera camera;
+    private boolean isRed = false; //What aliance are we on
 
     public AutoPaths(Follower follower,Camera camera){
         this.follower = follower;
         this.camera = camera;
     }
-    
+
+
     public void buildPathsPPG() {
         // basically just plotting the points for the lines that score the PPG pattern
         grabPPG = follower.pathBuilder() //
@@ -35,10 +42,18 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
                 .setLinearHeadingInterpolation(startPose.getHeading(), PPGPose.getHeading())
                 .build();
         // Move to the scoring pose from the first artifact pickup pose
-        scorePPG = follower.pathBuilder()
-                .addPath(new BezierLine(PPGPose, scorePose))
-                .setLinearHeadingInterpolation(PPGPose.getHeading(), scorePose.getHeading())
-                .build();
+        if (isRed) {
+            scorePPG = follower.pathBuilder()
+                    .addPath(new BezierLine(PPGPose, scorePoseRed ))
+                    .setLinearHeadingInterpolation(PPGPose.getHeading(), scorePoseRed.getHeading())
+                    .build();
+        } else {
+            scorePPG = follower.pathBuilder()
+                    .addPath(new BezierLine(PPGPose, scorePoseBlue))
+                    .setLinearHeadingInterpolation(PPGPose.getHeading(), scorePoseBlue.getHeading())
+                    .build();
+        }
+
     }
     public void buildPathsPGP() {
         // basically just plotting the points for the lines that score the PGP pattern
@@ -48,10 +63,17 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
                 .setLinearHeadingInterpolation(startPose.getHeading(), PGPPose.getHeading())
                 .build();
         // Move to the scoring pose from the first artifact pickup pose
-        scorePGP = follower.pathBuilder()
-                .addPath(new BezierLine(PGPPose, scorePose))
-                .setLinearHeadingInterpolation(PGPPose.getHeading(), scorePose.getHeading())
-                .build();
+        if (isRed) {
+            scorePGP = follower.pathBuilder()
+                    .addPath(new BezierLine(PGPPose, scorePoseRed))
+                    .setLinearHeadingInterpolation(PGPPose.getHeading(), scorePoseRed.getHeading())
+                    .build();
+        } else {
+            scorePGP = follower.pathBuilder()
+                    .addPath(new BezierLine(PGPPose, scorePoseBlue))
+                    .setLinearHeadingInterpolation(PGPPose.getHeading(), scorePoseBlue.getHeading())
+                    .build();
+        }
     }
     public void buildPathsGPP() {
         // basically just plotting the points for the lines that score the GPP pattern
@@ -61,16 +83,22 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
                 .setLinearHeadingInterpolation(startPose.getHeading(), GPPPose.getHeading())
                 .build();
         // Move to the scoring pose from the first artifact pickup pose
-        scoreGPP = follower.pathBuilder()
-                .addPath(new BezierLine(GPPPose, scorePose))
-                .setLinearHeadingInterpolation(GPPPose.getHeading(), scorePose.getHeading())
-                .build();
+        if(isRed){
+            scoreGPP = follower.pathBuilder()
+                    .addPath(new BezierLine(GPPPose, scorePoseRed))
+                    .setLinearHeadingInterpolation(GPPPose.getHeading(), scorePoseRed.getHeading())
+                    .build();
+        } else {
+            scoreGPP = follower.pathBuilder()
+                    .addPath(new BezierLine(GPPPose, scorePoseBlue))
+                    .setLinearHeadingInterpolation(GPPPose.getHeading(), scorePoseBlue.getHeading())
+                    .build();
+        }
     }
     //below is the state machine or each pattern
     public void updateStateMachinePPG() {
         switch (pathStatePPG) {
             case 0:
-            camera.setCamera(false);
                 // Move to the scoring position from the start position
                 follower.followPath(grabPPG);
                 setpathStatePPG(1); // Call the setter method
@@ -88,7 +116,6 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
     public void updateStateMachinePGP() {
         switch (pathStatePGP) {
             case 0:
-            camera.setCamera(false);
                 // Move to the scoring position from the start position
                 follower.followPath(grabPGP);
                 setpathStatePGP(1); // Call the setter method
@@ -106,7 +133,6 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
     public void updateStateMachineGPP() {
         switch (pathStateGPP) {
             case 0:
-            camera.setCamera(false);
                 // Move to the scoring position from the start position
                 follower.followPath(grabGPP);
                 setpathStateGPP(1); // Call the setter method
@@ -131,4 +157,5 @@ private final Pose startPose = new Pose(72, 120, Math.toRadians(90)); // Start P
     void setpathStateGPP(int newPathState) {
         this.pathStateGPP = newPathState;
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/subsystem/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/subsystem/Camera.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.xcentrics.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
@@ -8,6 +11,7 @@ import org.firstinspires.ftc.robotcore.external.android.util.Size;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.xcentrics.paths.AutoPaths;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
@@ -24,14 +28,14 @@ public class Camera extends SubsystemBase {
     private AprilTagProcessor aprilTag;
     private AprilTagDetection desiredTag = null;
     private TelemetryManager panlesTelemetry;
-    private static int foundID = null;
+    private static int foundID = -1;
     private static boolean cameraOn = true;
     private AutoPaths auto;
     private Follower follower;
     
     public Camera(HardwareMap hwMap, String name,Follower follower){
-        auto = new AutoPaths(follower);
-        panlesTelemetry = PanelsTelemetry.INSTANCE.getTelemerty();
+        auto = new AutoPaths(follower,this);
+        panlesTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
         aprilTag = new AprilTagProcessor.Builder().build();
         
@@ -47,7 +51,7 @@ public class Camera extends SubsystemBase {
 
     }
     public int foundID(){
-        if(foundID != null){
+        if(foundID != -1){
             return foundID;
         } else {
             return -1;
@@ -58,31 +62,38 @@ public class Camera extends SubsystemBase {
     }
     public void perodic(){
         if(cameraOn){
-         if (detection.metadata != null) {
+            // Step through the list of detected tags and look for a matching tag
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                // Look to see if we have size info on this tag.
+                if (detection.metadata != null) {
                     //  Check to see if we want to track towards this tag.
                     if (detection.id == PPG_TAG_ID) {
-                        auto.updateStateMachinePPG();
-                        targetFound = true;
+                        // call lines for the PGP pattern
+                        auto.buildPathsPPG();
+
                         desiredTag = detection;
                         foundID = 21; // This should likely be PPG_TAG_ID or the corresponding state machine ID
                         break;  // don't look any further.
                     } else if (detection.id == PGP_TAG_ID) {
-                        auto.updateStateMachinePGP();
-                        targetFound = true;
+                        // call lines for the PGP pattern
+                        auto.buildPathsPGP();
                         desiredTag = detection;
                         foundID = 22; // This should likely be PGP_TAG_ID or the corresponding state machine ID
                         break;  // don't look any further.
                     } else if (detection.id == GPP_TAG_ID) {
-                        auto.updateStateMachineGPP();
-                        targetFound = true;
+                        // call lines for the GPP pattern
+                        auto.buildPathsGPP();
+
                         desiredTag = detection;
                         foundID = 23; // This should likely be GPP_TAG_ID or the corresponding state machine ID
                         break;  // don't look any further.
                     }
                 } else {
                     // This tag is NOT in the library, so we don't have enough information to track to it.
-                    panlesTelemetry.debug("Unknown");
+                    panlesTelemetry.addData("Tag ID %d is not in TagLibrary", detection.id);
                 }
+            }
             }
     }
 
