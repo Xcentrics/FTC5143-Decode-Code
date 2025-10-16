258b62865deaec7da426171048d3375147721209
AlexanderStephens <alex.stephens@beasaint.org>
2025-10-16 17:35:37 +0000

    started to work on auto things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/paths/redAutoPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/paths/redAutoPaths.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.xcentrics.paths;
 
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+
 public class redAutoPaths {
+    private final Pose startPose = new Pose(72,120,Math.toRadians(90));
+    private final Pose scorePose = new Pose();
+    private final Pose PPGPose = new Pose();
+    private final Pose PGPPose = new Pose();
+    private final Pose GPPPose = new Pose();
+
+    private PathChain getPPG;
+    private PathChain scorePPG;
+    private PathChain getPGP;
+    private Pathchain scoreGPG;
+    private PathChain getGPP;
+    private PathChain scoreGPP;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/subsystem/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/subsystem/Camera.java
@@ -16,31 +16,60 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
 
 public class Camera extends SubsystemBase {
-
-    private AprilTagProcessor aprilTag;
+    
+    private static final int PPG_TAG_ID = 23;
+    private static final int PGP_TAG_ID = 22;
+    private static final int GPP_TAG_ID = 21;
     private VisionPortal visionPortal;
-    public List<AprilTagDetection> curretnDetections = aprilTag.getDetections();
-    boolean vision = true,flag = false;
+    private AprilTagProcessor aprilTag;
+    private AprilTagDetection desiredTag = null;
+    private TelemetryManager panlesTelemetry;
+    private static int foundID;
+    private static boolean cameraOn = true;
+    
+    public Camera(HardwareMap hwMap, String name){
+        panlesTelemetry = PanelsTelemetry.INSTANCE.getTelemerty();
 
-    public void vision(boolean b){
-        vision = b;
-    }
-    public Camera(HardwareMap hardwareMap, String name){
-        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
-        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class,"Webcam 1"), aprilTag);
+        aprilTag = new AprilTagProcessor.Builder().build();
+        
+        //Adjust image decimation to trade-off detection-range for decetion rate
+        aprilTag.setDecimation(2);
+
+        //build vison portal
+        visionPortal = new VisionPortal.Builder()
+        .setCamera(hwMap.get(WebcamName.class,"Webcam 1"))
+        .addProcessor(aprilTag)
+        .build();
+
+
     }
 
-    public List<AprilTagDetection> tagDetections(){
-        return curretnDetections;
+    public void perodic(){
+        if(cameraOn){
+         if (detection.metadata != null) {
+                    //  Check to see if we want to track towards this tag.
+                    if (detection.id == PPG_TAG_ID) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        foundID = 21; // This should likely be PPG_TAG_ID or the corresponding state machine ID
+                        break;  // don't look any further.
+                    } else if (detection.id == PGP_TAG_ID) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        foundID = 22; // This should likely be PGP_TAG_ID or the corresponding state machine ID
+                        break;  // don't look any further.
+                    } else if (detection.id == GPP_TAG_ID) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        foundID = 23; // This should likely be GPP_TAG_ID or the corresponding state machine ID
+                        break;  // don't look any further.
+                    }
+                } else {
+                    // This tag is NOT in the library, so we don't have enough information to track to it.
+                    panlesTelemetry.debug("Unknown");
+                }
+            }
     }
 
-    @Override
-    public void periodic(){
-        if(vision && flag){
-            visionPortal.resumeStreaming();
-        } else if(!vision && !flag) {
-            flag = true;
-            visionPortal.stopStreaming();
-        }
-    }
+
 }
